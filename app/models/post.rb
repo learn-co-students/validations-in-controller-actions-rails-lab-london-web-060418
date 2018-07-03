10a160f3ae8574fb273cf351146a5dbb5e93f433
ioreka <lucymhodges@gmail.com>
2018-07-03 09:53:23 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :content, length: { minimum: 100 }
-# 
-#   def valid_category
-#   if self.category
-#     cat_ary = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-#     if  !click_ary.any? { |phrase| self.title.include?(phrase)  }
-#       errors.add(:title, "Sorry pal, that title isn't clickbait enough")
-#     end
-#   end
-# end
-#
+
 
 end
